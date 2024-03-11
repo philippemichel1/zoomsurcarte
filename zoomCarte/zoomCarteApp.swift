d1bfbf9a1e82a78050dd9ffacdb89b99a2604768
Philippe MICHEL <philippe.michel1@gmail.com>
2024-03-11 15:37:40 +0100

    Initial Commit

---
+++ zoomCarte/zoomCarteApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoomCarteApp.swift
+//  zoomCarte
+//
+//  Created by Philippe MICHEL on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct zoomCarteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
